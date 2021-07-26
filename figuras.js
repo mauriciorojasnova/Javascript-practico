442ac5d42324a4f88918e17786207dac5ea771d3
mauriciorojasnova <andrsnov@gmail.com>
2021-07-26 17:21:20 -0500

    Agregué las fórmulas del círculo y del triángulo isóceles

--- figuras.js
+++ figuras.js
@@ -25,7 +25,27 @@ function areaTriangulo(baseTriangulo, alturaTriangulo) {
 
 console.groupEnd();
 
+//Código triángulo isóceles.
+
+console.group("Triángulos isóceles");
+
+function determinarTipo(ladoTrianguloIso1, ladoTrianguloIso2) {
+    if (ladoTrianguloIso1 === ladoTrianguloIso2){
+        return "Es un triángulo isóceles";
+    } else {
+        return "No es un triángulo isóceles";
+    }
+}
+
+function calcularAlturaTriangulo(ladoTrianguloIso1, baseTrianguloIso) {
+    const altura = Math.sqrt(ladoTrianguloIso1**2 - ((baseTrianguloIso/2)**2))
+    return altura;
+}
+
+console.groupEnd();
+
 //Código del círculo
+
 console.group("Círculos");
 
 function diametroCirculo(radioCirculo) {
@@ -40,7 +60,7 @@ function perimetroCirculo(radioCirculo) {
 }
 
 function areaCirculo(radioCirculo) {
-    return PI*(radioCirculo**radioCirculo);
+    return PI*(radioCirculo*radioCirculo);
 }
 
 console.groupEnd();
@@ -80,5 +100,41 @@ function calcularAreaTriangulo() {
     const valueAltura = inputAltura.value;
     const valueBase = inputBase.value;
     const area = areaTriangulo(valueBase, valueAltura);
+    alert(area);
+}
+
+//Interacción HTML triángulo Isóceles.
+
+function determinarTipoTriangulo() {
+    const inputLadoUnoIso = document.getElementById("InputLadoUnoIso");
+    const inputLadoDosIso = document.getElementById("InputLadoDosIso");
+    const valueIso1 = Number(inputLadoUnoIso.value);
+    const valueIso2 = Number(inputLadoDosIso.value);
+    const tipoTriangulo = determinarTipo(valueIso1, valueIso2);
+    alert(tipoTriangulo)
+}
+
+function determinarAlturaTriangulo() {
+    const inputLadoUnoIso = document.getElementById("InputLadoUnoIso");
+    const inputBaseIso = document.getElementById("InputBaseIso");
+    const valueIso1 = Number(inputLadoUnoIso.value);
+    const valueBaseIso = Number(inputBaseIso.value);
+    const altura = calcularAlturaTriangulo(valueIso1, valueBaseIso);
+    alert(altura);
+}
+
+//Interacción HTML círculo.
+
+function calcularPerimetroCirculo() {
+    const input = document.getElementById("InputCirculo");
+    const value = input.value;
+    const perimetro = perimetroCirculo(value);
+    alert(perimetro);
+}
+
+function calcularAreaCirculo() {
+    const input = document.getElementById("InputCirculo");
+    const value = input.value;
+    const area = areaCirculo(value);
     alert(area);
 }
