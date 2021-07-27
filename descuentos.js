e399c5170ae3a7ff9485ef2715a8430c28566743
mauriciorojasnova <andrsnov@gmail.com>
2021-07-26 20:31:35 -0500

    Añadí innerText con el valor del descuento del producto

--- descuentos.js
+++ descuentos.js
@@ -5,3 +5,18 @@ function calcularPrecioConDescuento(precio, descuento) {
     const precioConDescuento = precio*porcentajePrecioDescuento/100;
     return precioConDescuento;
 }
+
+//Unión con HTML
+
+function priceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const finalPriceMessage = document.getElementById("finalPriceMessage");
+    finalPriceMessage.innerText = "El precio con descuento es: $" + precioConDescuento;
+
+}
