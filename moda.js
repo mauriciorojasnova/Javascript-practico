1672864c645a873080f1fab73df87a3e588e503d
mauriciorojasnova <andrsnov@gmail.com>
2021-07-29 17:50:14 -0500

    Modifiqué función de moda y creé archivos de salarios

--- moda.js
+++ moda.js
@@ -1,28 +1,19 @@
-const lista = [
-    1,
-    2,
-    3,
-    4,
-    1,
-    2,
-    2
-];
+function calcularModa(lista) {
+  const listaCount = {};
 
-const listaCount = {};
-
-lista.map(
-    function (elemento){
-        if (listaCount[elemento]) {
-            listaCount[elemento] += 1;
-        } else {
-        listaCount[elemento] = 1;
-        }
+  lista.map(function (elemento) {
+    if (listaCount[elemento]) {
+      listaCount[elemento] += 1;
+    } else {
+      listaCount[elemento] = 1;
     }
-);
+  });
 
-const listaArray = Object.entries(listaCount.sort(
-    function (valorAcumulado, nuevoValor) {
-        return valorAcumulado[1] - nuevoValor;
-    }
-);
-console.log(listaArray)
+  const listaArray = Object.entries(listaCount).sort(function (
+    valorAcumulado,
+    nuevoValor
+  ) {
+    return valorAcumulado[1] - nuevoValor[1];
+  });
+  return moda = listaArray[listaArray.length - 1];
+}
